422b3060cd86d2a131eca47055995fdcb813c1bf
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-06-01 13:43:26 +0530

    Expose a C API to libdaylight

---
+++ capi/daylight.cpp
@@ -0,0 +1,23 @@
+#include "daylight_c.h"
+#include <libdaylight/Sunclock.hpp>
+
+extern "C" {
+Sunclock *newSunclock(double const latitude_, double const longitude_,
+                      double const tz_offset_) {
+  return new Sunclock(latitude_, longitude_, tz_offset_);
+}
+
+double Sunclock_irradiance(Sunclock *s, time_t when) {
+  return s->irradiance(when);
+}
+
+time_t Sunclock_sunrise(Sunclock *s, time_t date) { return s->sunrise(date); }
+
+time_t Sunclock_solar_noon(Sunclock *s, time_t date) {
+  return s->solar_noon(date);
+}
+
+time_t Sunclock_sunset(Sunclock *s, time_t date) { return s->sunset(date); }
+
+void deleteSunclock(Sunclock *s) { delete s; }
+}
---
+++ capi/daylight_c.h
@@ -0,0 +1,25 @@
+#ifndef __DAYLIGHT_H
+#define __DAYLIGHT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <time.h>
+
+typedef struct Sunclock Sunclock;
+
+Sunclock *newSunclock(double const latitude_, double const longitude_,
+                      double const tz_offset_);
+
+double Sunclock_irradiance(Sunclock *v, time_t when);
+time_t Sunclock_sunrise(Sunclock *v, time_t date);
+time_t Sunclock_solar_noon(Sunclock *v, time_t date);
+time_t Sunclock_sunset(Sunclock *v, time_t date);
+
+void deleteSunclock(Sunclock *v);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
