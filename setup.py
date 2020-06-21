4de90fca479b161fb24fe502bae0e2f0ff76dbcf
B Krishna Chaitanya <bkchaitan94@gmail.com>
2020-06-22 00:13:26 +0530

    Add setup.cfg for a more declarative python packaging structure

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import platform
 from setuptools import setup, Extension
-from tools.write_version_info import get_version_info
 
 
 class PyBind11Include:
@@ -18,36 +17,13 @@ else:
 
 
 setup(
-    name='daylight',
-    version=get_version_info()[3],
-    author='Krishna Chaitanya Bommakanti',
-    author_email='bkchaitan94@gmail.com',
-    description='Python bindings to libdaylight',
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Operating System :: OS Independent",
-        "Programming Language :: C",
-        "Programming Language :: C++",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Topic :: Home Automation",
-        "Topic :: Scientific/Engineering :: GIS",
-        "Topic :: Utilities",
-    ],
-    keywords='irradiance sunrise sunset sun geo gis',
     ext_modules=[
         Extension(
             'daylight',
-            ['source/Sunclock.cpp', 'pybind/source/daylight.cpp'],
+            [
+                'source/Sunclock.cpp',
+                'pybind/source/daylight.cpp'
+            ],
             include_dirs=[
                 'include',
                 'pybind/include',
@@ -57,8 +33,4 @@ setup(
             language='c++',
         ),
     ],
-    setup_requires=['pybind11~=2.5'],
-    install_requires=["numpy"],
-    extras_require={"docs": ["sphinx", "sphinx-rtd-theme", "numpydoc"]},
-    zip_safe=False,
 )
